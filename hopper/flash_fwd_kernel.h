b2a09fd6b61576c6b5a818ed6f0012320bbb959c
Jay Shah <jayhshah@gmail.com>
2024-09-04 11:22:56 -0700

    add in lse writeout and store zero

--- hopper/flash_fwd_kernel.h
+++ hopper/flash_fwd_kernel.h
@@ -175,13 +175,17 @@ __global__ void __launch_bounds__(Ktraits::kNWarps * cutlass::NumThreadsPerWarp,
                 }
             }
             int n_block_max = collective_mainloop.get_n_block_max(
-                mainloop_params, m_block, seqlen_traits_q, seqlen_traits_k);
-            if constexpr(!Seqlen_traits_Q::DecodingGQA) {
-                if ((Is_causal || seqlen_traits_k.UseVarSeqLen) && n_block_max <= 0) {  // We exit early and write 0 to gO and -inf to gLSE.
-                    collective_epilogue.store_zero(epilogue_params, shared_storage, threadIdx.x - NumCopyThreads, block_coord, seqlen_traits_q);
-                    continue;
+                mainloop_params, m_block, seqlen_traits_q, seqlen_traits_k);            
+            if ((Is_causal || seqlen_traits_k.UseVarSeqLen) && n_block_max <= 0) {  // We exit early and write 0 to gO and -inf to gLSE.
+                if constexpr(!Seqlen_traits_Q::DecodingGQA) {
+                    collective_epilogue.store_zero(epilogue_params, shared_storage, threadIdx.x - NumCopyThreads,
+                        block_coord, seqlen_traits_q);
+                } else {
+                    collective_epilogue.store_zero_decoding_gqa(epilogue_params, shared_storage, threadIdx.x - NumCopyThreads,
+                        block_coord, seqlen_traits_q, mainloop_params.qhead_per_khead_divmod);
                 }
-            }
+                continue;
+            }            
 
             collective_mainloop.mma(mainloop_params, pipeline_k, pipeline_v, smem_pipe_read_k, smem_pipe_read_v,
                                     tOrO, softmax, n_block_max, threadIdx.x - NumCopyThreads, work_idx, m_block, shared_storage,
